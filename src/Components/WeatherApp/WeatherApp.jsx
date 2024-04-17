4e507cb18c67d8f20c0e986e8df9aaac992d03db
hasuhasmath <hasuh5pepe@gmail.com>
2024-04-18 00:22:23 +0530

    initial

---
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./WeatherApp.css";
+import search_icon from "../Assests/search.png";
+import clear_icon from "../Assests/clear.png";
+import cloud_icon from "../Assests/cloud.png";
+import drizzle_icon from "../Assests/drizzle.png";
+import rain_icon from "../Assests/rain.png";
+import snow_icon from "../Assests/snow.png";
+import wind_icon from "../Assests/wind.png";
+import humidity_icon from "../Assests/humidity.png";
+
+const WeatherApp = () => {
+  return (
+    <div className="container ">
+      <div className="top-bar">
+        <input type="text" className="city-input" placeholder="Search" />
+        <div className="search-icon">
+          <img src={search_icon} alt="" />
+        </div>
+      </div>
+      <div className="weather-image">
+        <img src={cloud_icon} alt="" />
+      </div>
+      <div className="weather-temp">24</div>
+      <div className="weather-location">London</div>
+      <div className="data-container">
+        <div className="element">
+          <img src={humidity_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percent">64%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+        <div className="element">
+          <img src={wind_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percent">18 km/h</div>
+            <div className="text">Wind Speed</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherApp;
